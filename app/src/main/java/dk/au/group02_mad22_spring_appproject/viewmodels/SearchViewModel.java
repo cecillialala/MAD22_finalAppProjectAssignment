13783f0485088d9d3fa45aa41cfeabbbbe3cad3d
Spoon4k <murat@eroglu.dk>
2022-04-27 23:52:46 +0200

    SearchViewModel added and DB Model for Meal

---
+++ app/src/main/java/dk/au/group02_mad22_spring_appproject/viewmodels/SearchViewModel.java
@@ -0,0 +1,47 @@
+package dk.au.group02_mad22_spring_appproject.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.List;
+
+import dk.au.group02_mad22_spring_appproject.models.Meal;
+import dk.au.group02_mad22_spring_appproject.repository.Repository;
+
+// TODO: Repository need to be made
+public class SearchViewModel extends AndroidViewModel {
+
+    private Repository repository;
+    private LiveData<List<Meal>> mealsList;
+
+    public SearchViewModel(@NonNull Application app) {
+        super(app);
+        this.repository = Repository.getInstance(app);
+        this.mealsList = repository.getMealslist();
+    }
+
+    public void addMeal(Meal meal) {
+        repository.addMeal(meal);
+    }
+
+    public void setMeal(String mealName) {
+        repository.searchMeal(mealName, getApplication());
+    }
+
+    public ListenableFuture<Meal> findMeal(String mealName) {
+        return repository.findMeal(mealName);
+    }
+
+    public LiveData<List<Meal>> getMealsList() {
+        return mealsList;
+    }
+
+    public LiveData<List<Meal>> getCurrentMealsList() {
+        return repository.getCurrentMealslist();
+    }
+}
