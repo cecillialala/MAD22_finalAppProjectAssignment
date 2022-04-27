024c8733392fbdf034425bf007d8a6343ef8c129
FerhadRaskhi <50105976+FerhadRaskhi@users.noreply.github.com>
2022-04-27 23:07:15 +0200

    Register and login

--- app/src/main/java/dk/au/group02_mad22_spring_appproject/activities/loginactivity/Register.java
+++ app/src/main/java/dk/au/group02_mad22_spring_appproject/activities/loginactivity/Register.java
@@ -1,16 +1,102 @@
 package dk.au.group02_mad22_spring_appproject.activities.loginactivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import dk.au.group02_mad22_spring_appproject.R;
+import dk.au.group02_mad22_spring_appproject.activities.mainactivity.MainActivity;
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    EditText mFullName,mEmail,mPassword,mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+   // ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFullName   = findViewById(R.id.register_pt_nameID);
+        mEmail      = findViewById(R.id.login_pt_mailID);
+        mPassword   = findViewById(R.id.login_pt_passwordID);
+       // mPhone      = findViewById(R.id.phone);
+        mRegisterBtn= findViewById(R.id.register_btnID);
+        mLoginBtn   = findViewById(R.id.LoginAcount_Reg_text);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+              //  final String phone = mPhone.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = fAuth.getCurrentUser();
+                                    Toast.makeText(Register.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                   Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
--- app/src/main/java/dk/au/group02_mad22_spring_appproject/repository/Repository.java
+++ app/src/main/java/dk/au/group02_mad22_spring_appproject/repository/Repository.java
@@ -1,4 +1,41 @@
 package dk.au.group02_mad22_spring_appproject.repository;
 
-public class Repository {
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.function.Consumer;
+
+public class Repository extends AppCompatActivity {
+
+    private FirebaseLoginWithEmailAuth emailAuth;
+
+
+
+    //region Authentication
+
+    public boolean isLoggedIn() {
+        return emailAuth.isLoggedIn();
+    }
+
+
+    public void createUserWithEmailAndPassword(String email, String password, Consumer<FirebaseUser> successCallback) {
+        emailAuth.createUserWithEmailAndPassword(email, password, successCallback);
+    }
+
+    public void signInWithEmailAndPassword(String email, String password, Consumer<FirebaseUser> successCallback, Consumer<Exception> errorCallback) {
+        emailAuth.signInWithEmailAndPassword(email, password, successCallback, errorCallback);
+    }
+
+    public void signOut() {
+        emailAuth.signOut();
+    }
+
+    public void forgotPassword(String email) {
+        emailAuth.forgotPassword(email);
+    }
+
 }
